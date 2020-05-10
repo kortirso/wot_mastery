3a2946f3bede6ce289d13224c8f33895ae32e342
kortirso <kortirso@gmail.com>
2020-05-10 22:05:14 +0300

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,8 @@
   Country.create(name: { 'en' => country[0], 'ru' => country[1] })
 end
 # rubocop: enable Style/WordArray
+
+ussr = Country.find_by(name: { 'en' => 'USSR', 'ru' => 'СССР' })
+
+is7 = Tank.create(name: { 'en' => 'USSR', 'ru' => 'СССР' }, tier: 10, country: ussr, type: 2)
+Tanks::External.create(tank: is7, source: 0, external_id: '364-1046')
