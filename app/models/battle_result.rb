d3ff884ca8f6f3f87a56d333b5ce2b02df9a7072
kortirso <kortirso@gmail.com>
2020-05-11 23:30:46 +0300

    modify service

--- app/models/battle_result.rb
+++ app/models/battle_result.rb
@@ -3,6 +3,12 @@
 class BattleResult < ApplicationRecord
   include Sourceable
 
+  WIN_TITLE = 'Победа'
+  MASTER_GRADE = 'Мастер'
+  FIRST_GRADE = '1 класс'
+  SECOND_GRADE = '2 класс'
+  THIRD_GRADE = '3 класс'
+
   belongs_to :tank
 
   enum medal: { third_grade: 0, second_grade: 1, first_grade: 2, master: 3 }
--- app/services/fetcher/wot_replays.rb
+++ app/services/fetcher/wot_replays.rb
@@ -5,19 +5,20 @@ require 'open-uri'
 
 module Fetcher
   class WotReplays
-    attr_reader :tank, :external_id, :source, :page
+    attr_reader :tank, :tank_external_id, :source, :page
 
     BASE_URL = 'http://wotreplays.ru'
+    PAGE_LIMIT = 10
 
     def initialize(tank:)
-      @tank        = tank
-      @external_id = tank.wot_replays_external.external_id
-      @source      = 0
-      @page        = 1
+      @tank             = tank
+      @tank_external_id = tank.wot_replays_external.external_id
+      @source           = 0
+      @page             = 1
     end
 
     def call
-      while page <= 10
+      while page <= PAGE_LIMIT
         read_page
         @page += 1
       end
@@ -26,42 +27,50 @@ module Fetcher
     private
 
     def read_page
-      Nokogiri::HTML(URI.open(page_url)).css('.r_list.initial > li.clearfix').each do |item|
-        @item = item
-        next if BattleResult.find_by(external_id: battle_result_external_id)
+      Nokogiri::HTML(URI.open(page_url)).css('ul.r_list > li.clearfix').each do |item|
+        @item             = item
+        @link_for_details = link_for_battle_details
+        next unless @link_for_details
 
+        @external_id = battle_external_id
+        next if BattleResult.find_by(external_id: @external_id)
+
+        medal_info_block
         BattleResult.create!(battle_result_params)
       end
     end
 
     def page_url
-      "#{BASE_URL}/site/index/version/83/tank/#{external_id}/sort/uploaded_at.desc/page/#{page}/"
+      "#{BASE_URL}/site/index/version/83/tank/#{tank_external_id}/sort/uploaded_at.desc/page/#{page}/"
     end
 
-    def battle_result_external_id
-      @battle_result_external_id ||= link_for_details.split('#')[0].split('/')[-1]
+    def link_for_battle_details
+      @item.css('.r-map_85')[0]['href']
     end
 
-    def link_for_details
-      @link_for_details ||= @item.css('.r-map_85')[0]['href']
+    def battle_external_id
+      @link_for_details.split('#')[0].split('/')[-1]
     end
 
     def medal_info_block
-      return @medal_info_block if @medal_info_block
-
       info = Nokogiri::HTML(URI.open("#{BASE_URL}#{@link_for_details}"))
+      @title = info.css('.replay-stats__title')[0]
+      @date = info.css('.replay-stats__timestamp')[0]
       @medal_info_block = info.css('.replay-stats__medals tbody > tr:nth-of-type(1) > td:nth-of-type(1) img')
     end
 
     def battle_result_params
       {
-        experience: battle_result_param(2),
-        damage:     battle_result_param(4),
-        assist:     battle_result_param(5),
-        block:      battle_result_param(6),
-        tank:       tank,
-        source:     source,
-        medal:      medal_info_block.empty? ? nil : medal_info
+        experience:  battle_result_param(2),
+        damage:      battle_result_param(4),
+        assist:      battle_result_param(5),
+        block:       battle_result_param(6),
+        tank:        tank,
+        source:      source,
+        medal:       @medal_info_block.empty? ? nil : medal_info,
+        win:         @title == BattleResult::WIN_TITLE,
+        date:        @date.nil? ? '' : Date.parse(@date),
+        external_id: @external_id
       }
     end
 
@@ -70,11 +79,11 @@ module Fetcher
     end
 
     def medal_info
-      case medal_info_block[0]['title']
-      when 'Мастер' then 3
-      when '1 класс' then 2
-      when '2 класс' then 1
-      when '3 класс' then 0
+      case @medal_info_block[0]['title']
+      when BattleResult::MASTER_GRADE then 3
+      when BattleResult::FIRST_GRADE then 2
+      when BattleResult::SECOND_GRADE then 1
+      when BattleResult::THIRD_GRADE then 0
       end
     end
   end
