31238e85d8f3c4f3cd811f110f20571612f242db
kortirso <kortirso@gmail.com>
2020-05-23 23:09:51 +0300

    modify styles for tanks list

--- app/models/tanks/type.rb
+++ app/models/tanks/type.rb
@@ -8,6 +8,8 @@ module Tanks
 
     has_many :tanks, foreign_key: :tanks_type_id, inverse_of: :tanks_type, dependent: :destroy
 
+    enum name: { light: 0, medium: 1, heavy: 2, destroyer: 3, spg: 4 }
+
     validates :name, presence: true
   end
 end
